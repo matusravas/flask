42cd628683bba054fc5329b9f7c0a794a312d48c
Matus Ravas <matustabasco@gmail.com>
2020-11-04 01:03:30 +0100

    dumb flask app

--- app.py
+++ app.py
@@ -1,42 +1,63 @@
 from flask import Flask, Response, request
-# from flask.json import jsonify
+import csv
 import json
 app = Flask(__name__)
 
-#! asyncio gather, run_until_finish
-#! coroutines
+DB_DATA = []
+fieldnames = ['id','name', 'age', 'salary']
 
-data = [{'id': 1, 'name': 'Kebab', 'age': 19, 'salary': 1001010},
-        {'id': 2, 'name': 'Tibor', 'age': 20, 'salary': 1001010},
-        {'id': 3, 'name': 'Matus', 'age': 21, 'salary': 1001010},
-        {'id': 4, 'name': 'Ivan', 'age': 22, 'salary': 1001010}]
+# Uncomment below to generate user.csv
+ 
+# data = [{'id': 1, 'name': 'Kebab', 'age': 19, 'salary': 1000},
+#         {'id': 2, 'name': 'Tibor', 'age': 20, 'salary': 2000},
+#         {'id': 3, 'name': 'Matus', 'age': 21, 'salary': 3000},
+#         {'id': 4, 'name': 'Ivan', 'age': 22, 'salary': 4000}]
+
+# with open('users.csv', 'w', newline='') as f:
+#         writer = csv.DictWriter(f, fieldnames, delimiter='\t')
+#         writer.writeheader()
+#         for row in data:
+#             writer.writerow(row)
 
 
-def get_data_from_db():
-    return data
+def write_db_data_to_csv():
+    with open('users.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames, delimiter='\t')
+        writer.writeheader()
+        for row in DB_DATA:
+            writer.writerow(row)
 
 
-def get_user_from_db_by_id(request_data):
-    # connect to db and get data 
-    # sql = f'Select * from users where id == {id_}'
-    # call db
-    # return data
-    
-    id_ = int(request_data['id']) # {'id': '1'}
+def read_db_data_from_csv():
+    with open('.\\users.csv', 'r') as f:
+        reader = csv.DictReader(f, fieldnames, delimiter='\t')
+        next(reader) 
+        for row in reader:
+            DB_DATA.append(row)
+
+
+def get_user_from_db_by_id(request_data = None, user_id = None):
+    user_id = request_data['id'] if request_data else user_id
     result = {}
-    
-    for e in data:
-        if e['id'] == id_:
+    for e in DB_DATA:
+        if e['id'] == user_id:
             result = e
   
     return result
-            
 
-@app.route('/getUserByID_POST', methods=['POST'])
-def get_user_by_age_post():
-    request_data = request.get_json() # {'id': 1, 'salary': 0, 'foo': True, 'bar': {'foo': 'name'}, 'friends': ['Bob', 'Sam']}
-    # print(request_data)
+def insert_user_to_db(user):
+    current_id = 0
+    for row in DB_DATA:
+        if current_id <= int(row['id']):
+            current_id = int(row['id'])
+    user['id'] = current_id + 1
+    DB_DATA.append(user)
+    write_db_data_to_csv()
 
+
+@app.route('/getUserByID', methods=['POST'])
+def get_user_by_id_post():
+    request_data = request.get_json() 
     data = get_user_from_db_by_id(request_data)
     data = json.dumps(data)
     res = Response(data)
@@ -44,17 +65,18 @@ def get_user_by_age_post():
     return res
 
 
-@app.route('/getUserByID_GET', methods=['GET'])
-def get_user_by_age_get():
-    # http://localhost:5000/getUserByAge?age=20&name=Kebab
-    args = request.args # ([('age', '20'), ('name', 'Kebab')])
-    request_data = {}
-    for key, val in args.items():
-        request_data = {key: val}
-    # print(request_data)    
-    
-    data = get_user_from_db_by_id(request_data)
-    
+@app.route('/addUser', methods=['POST'])
+def add_user():
+    user = request.get_json() 
+    insert_user_to_db(user)
+    res = Response({f"User with ID {user['id']} successfully created"})
+    res.status_code = 201
+    return res
+
+
+@app.route('/getUserByID/<user_id>', methods=['GET'])
+def get_user_by_id_get(user_id):
+    data = get_user_from_db_by_id(user_id=user_id)
     data = json.dumps(data)
     res = Response(data)
     res.status_code = 200
@@ -62,4 +84,5 @@ def get_user_by_age_get():
 
 
 if __name__ == '__main__':
+    read_db_data_from_csv()
     app.run()
